f4637ac97005f360a44f91544d56ccf1968cedfb
Roger Coll <roger.coll.aumatell@gmail.com>
2021-05-05 17:54:29 +0200

    Sign/verify scheme

--- rsignatures.go
+++ rsignatures.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"math/big"
+	"reflect"
 )
 
 type Ring interface {
@@ -38,6 +39,10 @@ func (r *RSARing) encrypt(p, x *big.Int) *big.Int {
 	return new(big.Int).SetBytes(mHash)
 }
 
+func (ra *RSARing) g(s, p, n *big.Int) *big.Int {
+	return new(big.Int).Exp(s, p, n)
+}
+
 //returns the seed and the signatures of all ring members
 func (r *RSARing) Sign(ek []byte, round int) ([]byte, [][]byte, error) {
 	s := make([][]byte, len(r.ringKeys))
@@ -50,9 +55,27 @@ func (r *RSARing) Sign(ek []byte, round int) ([]byte, [][]byte, error) {
 	c := new(big.Int).Set(v)
 	for i := 0; i < len(r.ringKeys)-1; i++ {
 		if i != round {
-			//random created keys should sign the value here
+			randKey, err := randomBigInt()
+			if err != nil {
+				return nil, nil, err
+			}
+			s[i] = append(s[i], randKey.Bytes()...)
+			e := r.g(randKey, big.NewInt(int64(r.ringKeys[i].E)), r.ringKeys[i].N)
+			v = r.encrypt(ekp, new(big.Int).Xor(v, e))
 		}
 	}
+	sz := new(big.Int).Exp(r.encrypt(ekp, v), r.ringKeys[round].D, r.ringKeys[round].N)
+	s[round] = append(s[round], sz.Bytes()...)
 	return c.Bytes(), s, nil
 
+}
+
+func (r *RSARing) Verify(messHash, seed []byte, signatures [][]byte) bool {
+	result := new(big.Int).SetBytes(seed)
+	ekp := new(big.Int).SetBytes(seed)
+	for i, s := range signatures {
+		e := r.g(new(big.Int).SetBytes(s), big.NewInt(int64(r.ringKeys[i].E)), r.ringKeys[i].N)
+		result = r.encrypt(ekp, new(big.Int).Xor(ekp, e))
+	}
+	return reflect.DeepEqual(result.Bytes(), messHash)
 }
--- rsignatures_test.go
+++ rsignatures_test.go
@@ -1,14 +1,25 @@
 package rsignatures
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"testing"
 )
 
 func TestSign(t *testing.T) {
-	rsaRing := RSARing{}
-	v, _, err := rsaRing.Sign([]byte("hello"), 0)
+	partyKeys := make([]rsa.PrivateKey, 4)
+	for i, _ := range partyKeys {
+		randKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			t.Fatal(err)
+		}
+		partyKeys[i] = *randKey
+	}
+	rsaRing := RSARing{ringKeys: partyKeys}
+	seed, sig, err := rsaRing.Sign([]byte("hello"), len(partyKeys)-1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(v)
+	t.Log(seed)
+	t.Log(rsaRing.Verify([]byte("hello"), seed, sig))
 }
